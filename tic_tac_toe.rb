949bd1c5bb8ddb98653fbbdc41d62494e14091cb
Tristan Orlowski <tristan_orlowski@hotmail.com>
2021-09-15 19:00:52 -0400

    Create all classes and functions

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -22,6 +22,57 @@ class Grid
   def initialize
     @pos = (1..9).to_a
     @player = []
+  end
+
+  def play
+    gameover = false
+    moves = 0
+    i = 0
+
+    get_players
+    create_grid
+
+    until gameover
+      choice = get_choice(@player[i])
+      update(@player[i], choice)
+
+      if check_win?
+        gameover = true
+        win_msg(i)
+        puts 'GAMEOVER'
+        break
+      end
+
+      moves += 1
+      
+      if moves == 9
+        gameover = true
+        tie_msg
+        puts 'GAMEOVER'
+      end
+
+      player[i] == player.first ? i = 1 : i = 0
+    end
+  end
+
+  private
+
+  def create_grid
+    i = 0
+    lines = {out: "    :   :    \n", mid: "----:---:----\n", pos: line_pos(i)}
+    layout = [:out, :pos, :mid, :pos, :mid, :pos, :out]
+    layout.each do |l|
+      print lines[l]
+      lines[:pos] = line_pos(i += 3) if l == :mid
+    end
+  end
+ 
+  def line_pos(i)
+    "  #{@pos[i]} : #{@pos[i + 1]} : #{@pos[i + 2]}\n"
+  end
+
+  def update(player, position)
+    @pos[position.to_i - 1] = player.token
     create_grid
   end
 
@@ -47,40 +98,38 @@ class Grid
     end
   end
 
-  def update(player, position)
-    @pos[position.to_i - 1] = player.token
-    create_grid
-  end
-
   def check_win?
     WIN.any? { |w| [@pos[w[0]], @pos[w[1]], @pos[w[2]]].uniq.size == 1 }
   end
 
-  private
-
-  def create_grid
-    i = 0
-    lines = {out: "    :   :    \n", mid: "----:---:----\n", pos: line_pos(i)}
-    layout = [:out, :pos, :mid, :pos, :mid, :pos, :out]
-    layout.each do |l|
-      print lines[l]
-      lines[:pos] = line_pos(i += 3) if l == :mid
-    end
-  end
- 
-  def line_pos(i)
-    "  #{@pos[i]} : #{@pos[i + 1]} : #{@pos[i + 2]}\n"
-  end
-
   def wrong_move
-    puts 'Are you crazy! Try again.'
+    print 'Are you crazy! Try again: '
   end
 
-  def ask_choice(player)
-    puts "#{player}, make a decision. Choose a number from 1-9"
+  def win_msg(i)
+    puts "#{@player[i].name} is the winner! Suck it #{@player[i - 1].name}!"
   end
 
-  public
+  def tie_msg
+    puts 'You tied :( Try harder next time. Very disappointed in you both.'
+  end
+
+  def correct_choice?(choice)
+    choice.to_i.between?(1, 9) && !CHOICES.include?(@pos[choice - 1])
+  end
+
+  def get_choice(player)
+    choice = ''
+    print "#{player.name}, make a decision. Choose an empty space: "
+    until correct_choice?(choice)
+      choice = gets.chomp.to_i
+      unless correct_choice?(choice)
+        create_grid
+        wrong_move
+      end
+    end
+    choice
+  end
 
   def has_token?(i)
     CHOICES.include?(@pos[i - 1])
@@ -88,3 +137,4 @@ class Grid
 end
 
 grid = Grid.new
+grid.play
